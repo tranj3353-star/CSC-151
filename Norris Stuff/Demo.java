dda1d25a16a9be1b82863f5742564e04ce31f3f8
Justin Tran <tranj3353@ad.faytechcc.edu>
2026-02-05 13:58:46 -0500

    World's simplest crab game

--- Norris Stuff/Demo.java
+++ Norris Stuff/Demo.java
@@ -9,5 +9,23 @@ public class Demo {
         String name = scanner.nextLine();
         System.out.println();
         System.out.println("Nice to meetcha, " + name);
+
+        int chips = 100;
+        System.out.println(" well, " + name + " you start with " + chips + " chips");
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Rolling dice");
+            int roll = roll();
+            System.out.println("Rolled a " + roll);
+        }
+        
+        scanner.close();
+    }
+
+    public static int roll()
+    {
+        int random = (int) (Math.floor(Math.random() * 6)) + 1;
+        return random;
     }
 }
